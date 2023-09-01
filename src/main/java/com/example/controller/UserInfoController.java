39e57ccce62e7604283b13ad1cfbe313d346be52
Weixiao <weixiao.chen.98@foxmail.com>
2023-09-01 17:38:19 +0800

    using annotation in method parameter

--- src/main/java/com/example/controller/UserInfoController.java
+++ src/main/java/com/example/controller/UserInfoController.java
@@ -3,18 +3,22 @@ package com.example.controller;
 
 import com.example.bean.UserInfo;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.Default;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@Validated
 public class UserInfoController {
     @GetMapping("/user")
-    public String addUser(@Valid UserInfo userInfo, BindingResult bindingResult) {
+    public String addUser(@Validated(value = {UserInfo.Insert.class, Default.class}) UserInfo userInfo, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             List<ObjectError> allErrors = bindingResult.getAllErrors();
             for (ObjectError error : allErrors) {
@@ -30,5 +34,10 @@ public class UserInfoController {
             }
         }
         return "添加成功！";
+    }
+
+    @GetMapping("/username")
+    public String checkUsername(@NotNull String username) {
+        return "";
     }
 }
